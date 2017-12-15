220df58381714cd02b6ecb4da054f7cf0a20ee14
BackEndTea <gdpagter@ibuildings.nl>
2017-12-15 20:03:22 +0100

    Add tests for new exception type

--- src/Exception/KeyNotFoundException.php
+++ src/Exception/KeyNotFoundException.php
@@ -4,6 +4,13 @@ declare(strict_types=1);
 
 namespace BackEndTea\ArrayMeta\Exception;
 
-class KeyNotFoundException extends \RuntimeException
+final class KeyNotFoundException extends \RuntimeException
 {
+    public static function keyNotFound($key): self
+    {
+        return new self(\sprintf(
+            'Key "%s" not set.',
+            $key
+        ));
+    }
 }
